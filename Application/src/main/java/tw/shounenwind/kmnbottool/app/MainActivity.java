fb608ad5f0fce3ffc643864bd502ea8d886beaf7
ShounenWind <k236michaeimm@gmail.com>
2016-01-18 21:55:37 +0800

    layout

--- Application/src/main/java/tw/shounenwind/kmnbottool/app/MainActivity.java
+++ Application/src/main/java/tw/shounenwind/kmnbottool/app/MainActivity.java
@@ -3,13 +3,16 @@ package tw.shounenwind.kmnbottool.app;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.Proxy;
+import java.net.URL;
 
 import tw.shounenwind.kmnbottool.R;
 
@@ -39,12 +42,40 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void getBotData(){
+        try {
+            URL url = new URL("http://133.130.102.121:9163/petInfos/8433188.json");
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection(Proxy.NO_PROXY);
+            httpURLConnection.setUseCaches(false);
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+            BufferedReader bufferedReader;
+            if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8"));
+            }else{
+                bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream(), "UTF-8"));
+            }
+
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+            bufferedReader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     // BEGIN_INCLUDE(create_menu)
     /**
      * Use this method to instantiate your menu, and add your items to it. You
      * should return true if you have added items to it and want the menu to be displayed.
      */
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate our menu from the resources by using the menu inflater.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -58,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         MenuItemCompat.setShowAsAction(locationItem, MenuItem.SHOW_AS_ACTION_IF_ROOM);
 
         return true;
-    }
+    }*/
     // END_INCLUDE(create_menu)
 
     // BEGIN_INCLUDE(menu_item_selected)
@@ -67,13 +98,13 @@ public class MainActivity extends AppCompatActivity {
      * can be on the Action Bar, the overflow menu, or the standard options menu. You
      * should return true if you handle the selection.
      */
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             //TODO: menu item
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
     // END_INCLUDE(menu_item_selected)
 }
